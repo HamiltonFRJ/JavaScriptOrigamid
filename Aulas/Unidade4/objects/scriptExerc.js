01353f2e5710aace65cd7a7f0da9c4f411fc875c
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-07-31 23:53:04 -0300

    objectsExerc

---
+++ Aulas/Unidade4/objects/scriptExerc.js
@@ -0,0 +1,26 @@
+// Crie uma função que verifique corretamente o tipo de dado
+const verificaVariavel = (variavel) => {
+  return Object.prototype.toString.call(variavel);
+};
+
+// Crie um objeto quadrado com a propriedade lados e torne ela imutável
+const quadrado = {};
+Object.defineProperties(quadrado, {
+  lado: {
+    value: 4,
+    enumerable: true,
+  },
+});
+
+// Previna qualquer mudança no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: "#333",
+};
+
+Object.freeze(configuracao);
+
+// Liste o nome de todas as propriedades do protótipo de String e Array
+console.log(Object.getOwnPropertyNames(String.prototype));
+console.log(Object.getOwnPropertyNames(Array.prototype));
