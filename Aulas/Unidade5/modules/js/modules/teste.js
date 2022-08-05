4a35925b8c1e6e157ac7e4566654de224866a738
Hamilton Francisco Ramos Junior <hjunior@blossomconsult.com>
2022-08-05 11:15:56 -0300

    modules1

---
+++ Aulas/Unidade5/modules/js/modules/teste.js
@@ -0,0 +1,7 @@
+export function teste1(){
+  console.log("Olá amigo");
+}
+
+export function teste2(){
+  console.log("Olá companheiro");
+}
--- Aulas/Unidade5/modules/js/script.js
+++ Aulas/Unidade5/modules/js/script.js
@@ -2,8 +2,16 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import {teste1 as tt1, teste2 as tt2} from "./modules/teste.js"
+import * as novoTeste from "./modules/novoTeste.js"
+
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+tt1();
+tt2();
+novoTeste.estudo();
+
+
