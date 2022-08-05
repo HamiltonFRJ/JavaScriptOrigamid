d7cb1a37ca112e60ce6021133a0baa7800cd97cc
Hamilton Francisco Ramos Junior <hjunior@blossomconsult.com>
2022-08-05 11:43:23 -0300

    modules2

--- Aulas/Unidade5/modules/js/modules/novoTeste.js
+++ Aulas/Unidade5/modules/js/modules/novoTeste.js
@@ -1,3 +1,8 @@
 export function estudo(){
   console.log("Aprendendo");
 }
+
+export const ano = 2000;
+export const obj = {nome: 'Ford'};
+export const str = "Hamilton";
+export class carro {};
--- Aulas/Unidade5/modules/js/script.js
+++ Aulas/Unidade5/modules/js/script.js
@@ -13,5 +13,35 @@ initTabNav();
 tt1();
 tt2();
 novoTeste.estudo();
+console.log(novoTeste.obj);
 
+// Valores do export 
+// Podemos expotar objetos, funções, classes, números, strings e mais.
+// export const ano = 2000;
+// export const obj = {nome: 'Ford'};
+// export const str = "Hamilton";
+// export class carro {};
+
+/* Características
+* Strict Mode
+* 'use' strict mode por padrão em todos os arquivos
+*
+* Variáveis ficam no module apenas
+* Não vazam para o escopo globo.
+*
+* This fora de um objeto faz referência a undefined
+* Ao invez de fazer referência ao window.
+*
+* Assíncrono
+*/
+
+// Use Strict
+// O modo estrito previne algumas ações consideradas erros.
+// Basta adicionarmos 'use strict' no topo de um arquivo, que entrará neste modo.
+// nome = 'Ford'; // erro, variável global
+// delete Array.prototype; // erro, não deletável
+// window.top = 200; // erro, não pode mudar
+// const arguments = 3.14; // erro, escrito em palavra reservada
+
+// Por padrão todo module está no modo estrito
 
