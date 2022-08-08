0041f0e276b67fa945bae5d0c3fab6767bae2733
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-08-07 23:52:32 -0300

    tooltip-NotFinished1

--- Aulas/Unidade5/tooltip/js/modules/tooltip.js
+++ Aulas/Unidade5/tooltip/js/modules/tooltip.js
@@ -1,3 +1,18 @@
-export default function initTooltip{
-  
+export default function initTooltip() {}
+
+const tooltips = document.querySelectorAll("[data-tooltip]");
+
+tooltips.forEach((item) => {
+  item.addEventListener("mouseover", onMouseOver);
+});
+
+function onMouseOver(event) {
+  criarTooltipBox(this);
+  console.log(event);
+}
+
+function criarTooltipBox() {
+  const tooltipBox = document.createElement("div");
+  const text = element.getAttribute("aria-label");
+  tooltipBox.classList.add("tooltip");
 }
