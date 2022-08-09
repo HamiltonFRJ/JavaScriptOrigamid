887af3ac52af5a6f3cd12bb5fd1885b77e7c89af
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-08-09 11:09:57 -0300

    tooltip

---
+++ Aulas/Unidade5/tooltip/js/modules/dropdown-menu.js
@@ -0,0 +1,17 @@
+export default function initDropdownMenu() {
+  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+
+  dropdownMenus.forEach((menu) => {
+    //menu.addEventListener('touchstart', handleClick);
+    //menu.addEventListener('click', handleClick);
+
+    ["touchstar", "click"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle("active");
+  }
+}
