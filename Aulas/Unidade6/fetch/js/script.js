e76e82815235579cccb7a59185dd5797bbedde49
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-08-21 14:13:02 -0300

    fetch2

--- Aulas/Unidade6/fetch/js/script.js
+++ Aulas/Unidade6/fetch/js/script.js
@@ -29,41 +29,100 @@
 // Um tipo de formato de dados muito utilizado com JavaSccrip é o JSON (JavaScript Object Notation), pelo fato dele possuir basicamente a mesma sintaxe que a de um objeto js..
 // .json() transforma um corpo em json em um objeto JavaScript
 
-const local = fetch("https://viacep.com.br/ws/01001000/json/");
+// const local = fetch("https://viacep.com.br/ws/01001000/json/");
 
-local
-  .then((response) => response.json())
-  .then((body) => {
-    const conteudo = document.querySelector(".conteudo");
-    conteudo.innerText = body.logradouro;
-    console.log(body);
-  });
+// local
+//   .then((response) => response.json())
+//   .then((body) => {
+//     const conteudo = document.querySelector(".conteudo");
+//     conteudo.innerText = body.logradouro;
+//     console.log(body);
+//   });
 
 // .text()
 // Podemos utilizar o .text() para diferentes formatos como txt, json, html, css, js e mais.
 
-const style = document.createElement("style");
+// const style = document.createElement("style");
 
-const estilo = fetch("./css/style.css");
+// const estilo = fetch("./css/style.css");
 
-estilo
-  .then((response) => response.text())
-  .then((body) => {
-    const conteudo = document.querySelector(".conteudo");
-    style.innerHTML = body;
-    conteudo.appendChild(style);
-  });
+// estilo
+//   .then((response) => response.text())
+//   .then((body) => {
+//     const conteudo = document.querySelector(".conteudo");
+//     style.innerHTML = body;
+//     conteudo.appendChild(style);
+//   });
 
 // HTML e TEXT
 // Podemos pegar um arquivo inteiro em HTML, transformar o corpo em texto e inserir em uma div com o innerHTML. A partir dai podemos manipular esses dados como um DOM qualquer.
 
-const sobre = fetch("./sobre.html");
-const div = document.createElement("div");
-sobre
-  .then((r) => r.text())
+// const sobre = fetch("./sobre.html");
+// const div = document.createElement("div");
+// sobre
+//   .then((r) => r.text())
+//   .then((body) => {
+//     div.innerHTML = body;
+//     const titulo = div.querySelector("h1");
+//     document.querySelector("h1").innerText = titulo.innerText;
+//     console.log(titulo);
+//   });
+
+// Aula 2
+
+//.blob()
+// Um blob é um tipo de objeto utilizado para representação de dados de um arquivo. O blob pode ser utilizado para transofrmarmos requisições de imagens por exemplo. O blob gerqa um URL único.
+
+const imagem = fetch("./imagem.png");
+
+imagem
+  .then((r) => r.blob())
   .then((body) => {
-    div.innerHTML = body;
-    const titulo = div.querySelector("h1");
-    document.querySelector("h1").innerText = titulo.innerText;
-    console.log(titulo);
+    const blobUrl = URL.createObjectURL(body);
+    const imagemDom = document.querySelector("img");
+    imagemDom.src = blobUrl;
   });
+
+// clone()
+// Ao utilizarmos os métodos acima, text, json e blob, a resposta é modificada. Por isso existe o método clone, caso você necessite trasnformar uma resposta em diferentes valores.
+
+// const local = fetch("https://viacep.com.br/ws/01001000/json/");
+
+// local
+//   .then((r) => {
+//     const r2 = r.clone();
+//     r.text().then((text) => {
+//       console.log(text);
+//     });
+//     r2.json().then((json) => {
+//       console.log(json);
+//     });
+//     console.log(r);
+//   })
+//   .then((body) => console.log(body));
+
+// .headers
+// É uma propriedade que possui os cabeçalhos da requisição. É um tipo de dado iterável então podemos utilizar o forEach para vermos cada um deles.
+
+const imagem2 = fetch("https://viacep.com.br/ws/01001000/json/");
+
+imagem2.then((response) => {
+  console.log(response);
+  response.headers.forEach(console.log);
+});
+
+// .status e .ok
+// Retorna o status da requisição. Se foi 404, 200, 202 e mais. .ok retorna um valor booelano sendo true para uma requisição de sucesso e false para uma sem sucesso.
+
+// .url .type
+// /url retorna o url da requisição. .type retorna o tipo da resposta.
+
+fetch("https://viacep.com.br/ws/01001000/json/").then((response) => {
+  console.log(response.type, response.url);
+});
+
+// types
+// basic: feito na mesma origem
+// cors: feito em url body pode estar disponível
+// erro: erro de conexão
+// opaque: no-cors, não permite acesso de outros sites
