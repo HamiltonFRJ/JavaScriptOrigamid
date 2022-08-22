4faa0be67535b2a1bf6bfd7057f9a55853e48b03
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-08-22 11:16:07 -0300

    ambConfig-Unidade-6

---
+++ AmbienteDeConfiguracao/06-javascript-assincrono/06-javascript-assincrono/0603-fetch/script-aula.js
@@ -0,0 +1,9 @@
+const imagem = fetch('https://www.google.com');
+
+
+imagem.then(response => {
+  console.log(response.type);
+  if(response.status === 404) {
+    console.log('Página não existe');
+  }
+});
