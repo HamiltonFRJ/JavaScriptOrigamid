a0bdcc7282ff6333e333913ec69ce0c6561f10f9
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-08-25 16:46:09 -0300

    async-await

--- Aulas/Unidade6/async-await/js/script.js
+++ Aulas/Unidade6/async-await/js/script.js
@@ -1,13 +1,37 @@
-async function puxarDados() {
-  try {
-    const dadosResponse = await fetch('./json/dados.json');
-    const dadosJSON = await dadosResponse.json();
+// async function puxarDados() {
+//   try {
+//     const dadosResponse = await fetch('./json/dados.json');
+//     const dadosJSON = await dadosResponse.json();
 
-    document.body.innerText = dadosJSON.aula;
-  } catch (erro) {
-    console.log(erro);
-  }
+//     document.body.innerText = dadosJSON.aula;
+//   } catch (erro) {
+//     console.log(erro);
+//   }
+// }
+
+// puxarDados();
+
+async function puxarDados() {
+  const dadosResponse = fetch('./json/dados.json');
+  const clienteResponse = fetch('./json/clientes.json');
+
+  const jsonDados = await (await dadosResponse).json();
+  const jsonClientes = await (await clienteResponse).json();
+
+  console.log(jsonDados);
+  console.log(jsonClientes);
 }
 
 puxarDados();
 
+async function asyncSemPromise() {
+  await new Promise(resolve => {
+    setTimeout(() => {
+      console.log('Depois de 1 segundo');
+      resolve();
+    }, 1000);
+    console.log('Acabou!')
+  })
+}
+
+asyncSemPromise();
