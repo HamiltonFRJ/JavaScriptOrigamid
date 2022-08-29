a04a60f3b8379e0bb0b6d6087434003baa3be41a
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-08-28 23:54:00 -0300

    history-api

--- Aulas/Unidade6/history-api/js/script.js
+++ Aulas/Unidade6/history-api/js/script.js
@@ -12,12 +12,23 @@ const links = document.querySelectorAll("a");
 function handleClick(event) {
   event.preventDefault();
   fetchPage(event.target.href);
+  window.history.pushState(null, null, event.target.href);
 }
 
 async function fetchPage(url) {
   const pageResponse = await fetch(url);
   const pageText = await pageResponse.text();
-  console.log(url);
+  replaceContent(pageText);
+}
+
+function replaceContent(newText) {
+  const newHtml = document.createElement("div");
+  newHtml.innerHTML = newText;
+  const oldContent = document.querySelector("content");
+  const newContent = newHtml.querySelector("content");
+
+  oldContent.innerHtml = newContent.innerHTML;
+  document.title = newHtml.querySelector("title").innerText;
 }
 
 links.forEach((link) => {
