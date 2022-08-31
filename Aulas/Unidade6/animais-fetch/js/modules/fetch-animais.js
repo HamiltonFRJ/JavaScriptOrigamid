4281f4909603dd62349accbc306384d81d72db08
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-08-31 16:28:02 -0300

    bitcoin-fetch

--- Aulas/Unidade6/animais-fetch/js/modules/fetch-animais.js
+++ Aulas/Unidade6/animais-fetch/js/modules/fetch-animais.js
@@ -2,15 +2,19 @@ import initAnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais() {
   async function fetchAnimais(url) {
-    const animaisResponse = await fetch(url);
-    const animaisJSON = await animaisResponse.json();
-    const numerosGrid = document.querySelector(".numeros-grid");
+    try {
+      const animaisResponse = await fetch(url);
+      const animaisJSON = await animaisResponse.json();
+      const numerosGrid = document.querySelector(".numeros-grid");
 
-    animaisJSON.forEach((animal) => {
-      const divAnimal = createAnimal(animal);
-      numerosGrid.appendChild(divAnimal);
-    });
-    initAnimaNumeros();
+      animaisJSON.forEach((animal) => {
+        const divAnimal = createAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+      });
+      initAnimaNumeros();
+    } catch (erro) {
+      console.log(erro);
+    }
   }
 
   function createAnimal(animal) {
