c4168187d4781b3286057801f60ee3129df71474
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-09-03 20:35:50 -0300

    classes-final

--- Aulas/Unidade7/classes/js/script.js
+++ Aulas/Unidade7/classes/js/script.js
@@ -14,6 +14,51 @@
 
 // console.log(blueBotton.element());
 
+// class Button {
+//   constructor(text, background, color) {
+//     this.text = text;
+//     this.background = background;
+//     this.color = color;
+//   }
+//   element() {
+//     const buttonElement = document.createElement("button");
+//     buttonElement.innerText = this.text;
+//     buttonElement.style.background = this.background;
+//     buttonElement.style.color = this.color;
+//     return buttonElement;
+//   }
+//   appendTo(target) {
+//     const targetElement = document.querySelector(target);
+//     targetElement.appendChild(this.element());
+//     return targetElement;
+//   }
+// }
+
+// const blueButton = new Button("Comprar", "red", "white");
+// console.log(blueButton.appendTo("body"));
+
+// class Button {
+//   constructor(options) {
+//     this.options = options;
+//   }
+//   static createButton(text, background) {
+//     const buttonElement = document.createElement("button");
+//     buttonElement.innerText = text;
+//     buttonElement.style.background = background;
+//     return buttonElement;
+//   }
+// }
+
+// const blueButton = new Button({
+//   backgroundColor: "Blue",
+//   text: "Comprar",
+//   color: "white",
+// });
+
+// const blueButtonStatic = Button.createButton("Clique", "blue");
+
+// console.log(blueButtonStatic);
+
 class Button {
   constructor(text, background, color) {
     this.text = text;
@@ -22,11 +67,19 @@ class Button {
   }
   element() {
     const buttonElement = document.createElement("button");
-    buttonElement.innerText = thjis.text;
+    buttonElement.innerText = this.text;
     buttonElement.style.background = this.background;
     buttonElement.style.color = this.color;
     return buttonElement;
   }
+  appendTo(target) {
+    const targetElement = document.querySelector(target);
+    targetElement.appendChild(this.element());
+    return targetElement;
+  }
+  static blueButton(text) {
+    return new Button(text, "blue", "white");
+  }
 }
 
-const blueButton = new Button("Comprar", "red", "white");
+const botaoAzul = Button.blueButton("Comprar");
