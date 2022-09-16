65de4941d9a35f7df193883b9440ecf5b604f1d6
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-09-16 15:25:52 -0300

    validar-cpf-2

--- Aulas/Unidade9/validar-cpf/js/modules/validar-cpf.js
+++ Aulas/Unidade9/validar-cpf/js/modules/validar-cpf.js
@@ -12,4 +12,25 @@ export default class ValidarCPF {
     const cpfLimpo = this.limpar(cpf);
     return this.construir(cpfLimpo);
   }
+  validar(cpf) {
+    const matchCPF = cpf.match(/(?:\d{3}[.-\s]?){3}\d{2}/g);
+    return (matchCPF && matchCPF[0] === cpf);
+  }
+  validarNaMudanca(cpfElement) {
+    if (this.validar(cpfElement.value)) {
+      cpfElement.value = this.formatar(cpfElement.value);
+    } else {
+
+    }
+  }
+  adicionarEvento() {
+    // !README: Linha 1 e 2 - anotacoes.txt
+    this.element.addEventListener("change", () => {
+      this.validarNaMudanca(this.element);
+    })
+  }
+  iniciar() {
+    this.adicionarEvento();
+    return this;
+  }
 }
--- Aulas/Unidade9/validar-cpf/js/script.js
+++ Aulas/Unidade9/validar-cpf/js/script.js
@@ -1,6 +1,4 @@
 import ValidarCPF from "./modules/validar-cpf.js";
 
 const cpf = document.querySelector("#cpf");
-const validarCPF = new ValidarCPF();
-
-console.log(validarCPF.formatar("1115553.3322"))
+const validarCPF = new ValidarCPF(cpf).iniciar();
